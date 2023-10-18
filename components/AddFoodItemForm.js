2096c41057bd78ab8f9fb4dbb9697f03dc83caff
Akshay200228 <akshayrs096@gmail.com>
2023-10-18 21:10:59 +0530

    improve form and add validation

--- components/AddFoodItemForm.js
+++ components/AddFoodItemForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, TouchableOpacity, StyleSheet, Animated } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 const AddFoodItemForm = ({ onSubmit, initialValues, onClose }) => {
@@ -7,12 +7,20 @@ const AddFoodItemForm = ({ onSubmit, initialValues, onClose }) => {
   const [foodPrice, setFoodPrice] = useState(initialValues.foodPrice || '');
   const [error, setError] = useState('');
 
+  const fadeAnim = new Animated.Value(0);
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }, []);
 
   const handleSubmit = () => {
     if (foodName.trim() === '' || foodPrice.trim() === '') {
       setError('Please fill in all fields.');
-    }
-    else if (isNaN(foodPrice) || foodPrice.trim() === '') {
+    } else if (isNaN(foodPrice) || foodPrice.trim() === '') {
       setError('Please enter a valid numeric Food Price.');
     } else {
       const newItem = {
@@ -27,13 +35,14 @@ const AddFoodItemForm = ({ onSubmit, initialValues, onClose }) => {
     }
   };
 
-
   return (
-    <View style={styles.formContainer}>
-      <TouchableOpacity style={styles.closeButton} onPress={onClose}>
-        <MaterialIcons name="close" size={24} color="black" />
-      </TouchableOpacity>
-      <Text style={styles.title}>Add Food Item</Text>
+    <Animated.View style={[styles.formContainer, { opacity: fadeAnim }]}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Add Food Item</Text>
+        <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+          <MaterialIcons name="close" size={24} color="red" />
+        </TouchableOpacity>
+      </View>
       <Text style={styles.errorText}>{error}</Text>
       <TextInput
         style={styles.input}
@@ -50,10 +59,10 @@ const AddFoodItemForm = ({ onSubmit, initialValues, onClose }) => {
           }
         }}
         placeholder="Food Price"
-        keyboardType="numeric" // Ensure the numeric keyboard
+        keyboardType="numeric"
       />
       <Button title="Submit" onPress={handleSubmit} />
-    </View>
+    </Animated.View>
   );
 };
 
@@ -62,7 +71,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 0,
     width: '100%',
-    backgroundColor: '#fff',
+    backgroundColor: '#9FE2BF', 
     paddingHorizontal: 20,
     paddingTop: 20,
     paddingBottom: 40,
@@ -73,26 +82,31 @@ const styles = StyleSheet.create({
     shadowRadius: 5,
     shadowOpacity: 0.2,
   },
+  titleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  closeButton: {
+    top: 4,
+    right: 10,
+  },
   errorText: {
     color: 'red',
     marginBottom: 10,
   },
-  closeButton: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
-  },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
+    color: '#fff', 
   },
   input: {
     width: '100%',
     paddingVertical: 10,
     paddingHorizontal: 15,
     marginBottom: 15,
-    backgroundColor: '#f7f7f7',
+    backgroundColor: '#fff',
+    color: '#333',
     borderRadius: 10,
   },
 });
