8fcfabb817465f55a7174882a6d3120e357626a5
Akshay200228 <akshayrs096@gmail.com>
2023-10-18 21:21:00 +0530

    Pending Drag and drop functionality

--- screens/FinalFoodList.js
+++ screens/FinalFoodList.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const FinalFoodList = ({ route }) => {
   const { foodItems } = route.params;
 
   return (
-    <View>
-      <Text>Final Food List</Text>
-      <Text>{JSON.stringify(foodItems)}</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Final Food List</Text>
+      <Text style={styles.foodItems}>{JSON.stringify(foodItems)}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'start',
+    justifyContent: 'start',
+    backgroundColor: '#fff',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  foodItems: {
+    fontSize: 16,
+  },
+});
 
 export default FinalFoodList;
